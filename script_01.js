8e69aa9e4f5e4f26e14aa7ca206d43242ad79123
Daniel <schindlerdaniel@web.de>
2022-11-10 12:32:27 +0100

    asdf

--- script_01.js
+++ script_01.js
@@ -40,37 +40,58 @@
 
 /************ Switch-Case ************/
 
-const firstName = "Bob";
-let job;
+// const firstName = "Bob";
+// let job;
 
 
 
-job = "driver";     // .. fährt TAXI! / UBER
-// job = "diver";      // .. taucht im Rhein!
-// job = "artist";     // .. malt ein Bild!
-// job = "pilot";      // .. macht etwas anderes! --> default
-// job = "teacher";    // .. unterrichtet!
-// job = "instructor";  // .. unterrichtet!
+// job = "driver";     // .. fährt TAXI! / UBER
+// // job = "diver";      // .. taucht im Rhein!
+// // job = "artist";     // .. malt ein Bild!
+// // job = "pilot";      // .. macht etwas anderes! --> default
+// // job = "teacher";    // .. unterrichtet!
+// // job = "instructor";  // .. unterrichtet!
 
-switch (job) {
-    case "driver":
-        console.log(firstName, "fährt Uber.");
-        break;
+// switch (job) {
+//     case "driver":
+//         console.log(firstName, "fährt Uber.");
+//         break;
 
-    case "diver":
-        console.log(firstName, "taucht im Main.");
-        break;
+//     case "diver":
+//         console.log(firstName, "taucht im Main.");
+//         break;
 
-    case "artist":
-        console.log(firstName, "malt ein Bild.");
-        break;
+//     case "artist":
+//         console.log(firstName, "malt ein Bild.");
+//         break;
     
-    case "teacher":
-    case "instructor":
-        console.log(firstName, "unterrichtet.");
-        break;
+//     case "teacher":
+//     case "instructor":
+//         console.log(firstName, "unterrichtet.");
+//         break;
 
+//     default:
+//         console.log(firstName, "ist", job)
+//         break;
+// }
+
+/********  Fallunterscheidung / SWITCH|CASE 2 *******/
+
+
+
+const cond = true;
+const a = 2;
+
+
+
+switch (cond) {
+    case (a == 1):
+        console.log("a ist gleich 1")
+        break;
+    case (a == 2):
+        console.log("a ist gleich 2")
+        break;
     default:
-        console.log(firstName, "ist", job)
+        console.log("a hat einen anderen Wert")
         break;
 }
